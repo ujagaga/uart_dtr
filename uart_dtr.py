1596663bb15d4fcdbb8c046053f2345567ae4add
Rada Berar <ujagaga@gmail.com>
2025-07-22 20:46:40 +0200

    Update

--- uart_dtr.py
+++ uart_dtr.py
@@ -2,6 +2,8 @@
 
 import serial
 import sys
+import time
+import errno
 
 
 port = "/dev/ttyUSB0"
@@ -15,15 +17,34 @@ if len(sys.argv) > 1:
 
 try:
     ser = serial.Serial(port, 9600)
+    # Often it does not work correctly the first time, so you need to try again
+    ser.close()
+    time.sleep(0.1)
+    ser = serial.Serial(port, 9600)
+
     ser.dtr = True
     print(f"DTR set LOW on port {port}")
-
     input("\nPress enter to set HIGH")
 
     ser.dtr = False
-
     print(f"DTR set HIGH on port {port}\n")
     ser.close()
+
 except serial.SerialException as e:
+    if hasattr(e, 'errno'):
+        err = e.errno
+    else:
+        # On some systems, errno might not be directly available.
+        # Parse the string instead:
+        err = None
+        if "Device or resource busy" in str(e):
+            err = errno.EBUSY
+
+    if err == errno.EBUSY:
+        print(f"Port {port} is busy (Device is open by another process).")
+    else:
+        print(f"SerialException: {e}")
+
+except Exception as e:
     print(f"Error opening or setting DTR on port {port}: {e}")
     print("Please specify a valid serial port")
